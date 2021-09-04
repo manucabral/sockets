9f6118d0939a181e9f0490a7c864c8b1800f9d2f
Manu <mcabral@frba.utn.edu.ar>
2021-09-04 06:03:50 -0300

    Agregado cliente en go.

---
+++ clientes/cliente.go
@@ -0,0 +1,21 @@
+package main
+
+import "os"
+import "net"
+import "fmt"
+import "bufio"
+
+func main() {
+
+  conn, _ := net.Dial("tcp", "127.0.0.1:8123")
+  for {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("> ")
+    text, _ := reader.ReadString('\n')
+
+    fmt.Fprintf(conn, text + "\n")
+
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    fmt.Print(">> "+message)
+  }
+}
