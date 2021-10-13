adef19f56ae573594dd8a727fb499e079b581e05
manucabral <83357673+manucabral@users.noreply.github.com>
2021-10-13 11:12:00 -0300

    Update cliente.go

--- clientes/cliente.go
+++ clientes/cliente.go
@@ -12,9 +12,7 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("> ")
     text, _ := reader.ReadString('\n')
-
     fmt.Fprintf(conn, text + "\n")
-
     message, _ := bufio.NewReader(conn).ReadString('\n')
     fmt.Print(">> "+message)
   }
